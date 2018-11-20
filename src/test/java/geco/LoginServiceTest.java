0ff8775f270d6540498f24e34ab3d744c96487e1
clement-amat <clemamat@gmail.com>
2018-11-20 10:41:04 +0100

    Ajout classe test LoginServiceTest

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,18 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginServiceTest {
+
+    
+
+}
