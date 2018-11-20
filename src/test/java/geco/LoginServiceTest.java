74e275a37d4c843540ca9cbffc71dac577f68323
clement-amat <clemamat@gmail.com>
2018-11-20 10:47:30 +0100

    #1 Couverture de test a 100pc pour LoginService

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -13,6 +13,45 @@ import static org.junit.Assert.assertTrue;
 
 public class LoginServiceTest {
 
-    
+    private String[] cinqLogins = {"a", "ab", "ccc", "dddd", "eeeee"};
+    private String[] emptyLogins = {};
+
+    private LoginService cingLoginService;
+    private LoginService emptyLoginService;
+
+    @Before
+    public void setUp() {
+        cingLoginService = new LoginService(cinqLogins);
+        emptyLoginService = new LoginService(emptyLogins);
+    }
+
+    @Test
+    public void loginExistsTestWithExistingLogin() {
+        assertTrue("Login existant non reconu", cingLoginService.loginExists(cinqLogins[0]));
+    }
+
+    @Test
+    public void loginExistsTestWithNonExistingLogin() {
+        assertFalse("Login non existant reconu", cingLoginService.loginExists(cinqLogins[0] + "/"));
+    }
+
+    @Test
+    public void addLoginTest() {
+        emptyLoginService.addLogin("toto");
+        assertEquals("Ajout d'un login probleme", 1, emptyLoginService.findAllLogins().size());
+    }
+
+    @Test
+    public void findAllLoginsStartingWithTestEmpty() {
+        assertEquals("Liste non vide quand aucun prefixe ne correspond",
+                Collections.EMPTY_LIST, emptyLoginService.findAllLoginsStartingWith("whatever"));
+    }
+
+    @Test
+    public void findAllLoginsStartingWithTestNonEmpty() {
+        List<String> result = new ArrayList<>();
+        Collections.addAll(result, "a", "ab");
+        assertEquals("Mauvais identifiants correspondants a un prefixe", result, cingLoginService.findAllLoginsStartingWith("a"));
+    }
 
 }
